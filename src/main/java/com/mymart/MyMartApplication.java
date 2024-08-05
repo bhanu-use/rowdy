498dc293382802fab177bf26481f472aa53c628c
bhanu <bhanuchander0436@gmail.com>
2024-08-05 11:59:50 +0530

    ok

--- src/main/java/com/mymart/MyMartApplication.java
+++ src/main/java/com/mymart/MyMartApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyMartApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyMartApplication.class, args); 	                                                                             
 	}
